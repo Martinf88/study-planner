438984c8d5b5a0913bbf5a74018d5927c7eee0eb
Martin Fahlström <makrill88@gmail.com>
2024-05-22 14:21:12 +0200

    nytt e2e test

--- cypress/e2e/edit-task.cy.js
+++ cypress/e2e/edit-task.cy.js
@@ -1,4 +1,4 @@
-describe("Edit tasks", () => {
+describe("Edit, remove and search tasks", () => {
   it("can edit task and save it and then remove it", () => {
     //surfa till webbsidan och kör testerna på den
     cy.visit("/");
@@ -18,5 +18,19 @@ describe("Edit tasks", () => {
     cy.get("[title='Ta bort']").each(($span) => {
       cy.wrap($span).click();
     });
+  });
+
+  it("should toggle todo item status when checkbox is clicked", () => {
+    cy.visit("/");
+    cy.get('.item input[type="checkbox"]').each(($checkbox) => {
+      cy.wrap($checkbox).click();
+    });
+    cy.get('.item input[type="checkbox"]').should("be.checked");
+  });
+
+  it("should be possible to search for a task", () => {
+    cy.visit("/");
+    cy.get("input[type='search']").should("be.visible").type("inlämning");
+    cy.get(".prio-items").children().its("length").should("eq", 2);
   });
 });
